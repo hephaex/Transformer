b11387bce187b4bbfbc229a6c622f92f86891684
Mario Cho <hephaex@gmail.com>
2023-03-30 04:59:41 +0900

    add class of mask():

--- transformer.py
+++ transformer.py
@@ -171,3 +171,19 @@ class Transformer(nn.Module):
 
     def decode(self, tgt, memory, mask_tgt):
         return self.transformer_decoder(self.positional_encoding(self.token_embedding_tgt(tgt)), memory, mask_tgt)    
+
+#
+def create_mask(src, tgt):
+    
+    seq_len_src = src.shape[1]
+    seq_len_tgt = tgt.shape[1]
+
+    mask_tgt = generate_square_subsequent_mask(seq_len_tgt).to(device)
+    mask_src = generate_square_subsequent_mask(seq_len_src).to(device)
+
+    return mask_src, mask_tgt
+
+
+def generate_square_subsequent_mask(seq_len):
+    mask = torch.triu(torch.full((seq_len, seq_len), float('-inf')), diagonal=1)
+    return mask                      
